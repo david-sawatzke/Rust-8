634291da64ea1d9734f6643783d26921213980e5
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-09-27 01:07:19 +0200

    Implement keypad reading

--- chip8-embedded/src/main.rs
+++ chip8-embedded/src/main.rs
@@ -14,7 +14,7 @@ use cortex_m_semihosting::hprintln;
 use embedded_hal::digital::v2;
 
 bitflags! {
-    struct RowKeys: u32 {
+    struct RowKeys: u8  {
         const A = 0b0001;
         const B = 0b0010;
         const C = 0b0100;
@@ -52,12 +52,9 @@ fn main() -> ! {
             );
 
             loop {
-                r1.set_low();
-                r2.set_low();
-                r3.set_low();
-                r4.set_low();
-                let row = read_row(&c1, &c2, &c3, &c4).unwrap();
-                hprintln!("{:?}, {:?}", row, c1.is_low()).unwrap();
+                let pressed_keys =
+                    read_keypad(&mut r1, &mut r2, &mut r3, &mut r4, &c1, &c2, &c3, &c4).unwrap();
+                hprintln!("{:?}, {:?}", pressed_keys, c1.is_low()).unwrap();
                 delay.delay_ms(1_000_u16);
             }
         });
@@ -66,6 +63,45 @@ fn main() -> ! {
     loop {
         continue;
     }
+}
+
+fn read_keypad<R1, R2, R3, R4, C1, C2, C3, C4, E>(
+    r1: &mut R1,
+    r2: &mut R2,
+    r3: &mut R3,
+    r4: &mut R4,
+    c1: &C1,
+    c2: &C2,
+    c3: &C3,
+    c4: &C4,
+) -> Result<u16, E>
+where
+    R1: v2::OutputPin<Error = E>,
+    R2: v2::OutputPin<Error = E>,
+    R3: v2::OutputPin<Error = E>,
+    R4: v2::OutputPin<Error = E>,
+    C1: v2::InputPin<Error = E>,
+    C2: v2::InputPin<Error = E>,
+    C3: v2::InputPin<Error = E>,
+    C4: v2::InputPin<Error = E>,
+{
+    r1.set_low()?;
+    r2.set_high()?;
+    r3.set_high()?;
+    r4.set_high()?;
+    let row1 = read_row(c1, c2, c3, c4)?.bits();
+    r1.set_high()?;
+    r2.set_low()?;
+    let row2 = read_row(c1, c2, c3, c4)?.bits();
+    r2.set_high()?;
+    r3.set_low()?;
+    let row3 = read_row(c1, c2, c3, c4)?.bits();
+    r3.set_high()?;
+    r4.set_low()?;
+    let row4 = read_row(c1, c2, c3, c4)?.bits();
+    r4.set_high()?;
+    let pressed_keys = row1 as u16 | (row2 as u16) << 4 | (row3 as u16) << 8 | (row4 as u16) << 12;
+    Ok(pressed_keys)
 }
 
 fn read_row<C1, C2, C3, C4, E>(c1: &C1, c2: &C2, c3: &C3, c4: &C4) -> Result<RowKeys, E>
