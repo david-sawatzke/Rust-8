e5a4dd583dfb09c0c4f0065c7bc4a5969b5f9ff5
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-09-29 21:52:48 +0200

    Add POC key handling

--- chip8-embedded/src/main.rs
+++ chip8-embedded/src/main.rs
@@ -47,6 +47,7 @@ fn main() -> ! {
                 Timer::tim16(p.TIM16, Hertz(chip8::INSTRUCTION_RATE), &mut rcc);
             let mut delay_timer = Timer::tim17(p.TIM17, Hertz(chip8::TIMER_RATE), &mut rcc);
             let mut computer = chip8::Chip8::new(game_data, random::RandomGen { state: 43 });
+            let mut pressed_key = None;
             loop {
                 if instruction_timer.wait().is_ok() {
                     computer.run_cycle();
@@ -54,6 +55,19 @@ fn main() -> ! {
                         keypad::read_keypad(&mut r1, &mut r2, &mut r3, &mut r4, &c1, &c2, &c3, &c4)
                             .unwrap();
                     hprintln!("{:?}, {:?}", pressed_keys, c1.is_low()).unwrap();
+                    let curr_pressed_key = pressed_keys.trailing_zeros() as u8;
+                    // TODO Expand for multiple pressed keys
+                    pressed_key = if curr_pressed_key == 16 {
+                        if let Some(prev_pressed_key) = pressed_key {
+                            computer.handle_key_release(prev_pressed_key);
+                        }
+                        None
+                    } else {
+                        if None == pressed_key {
+                            computer.handle_key_press(curr_pressed_key);
+                        }
+                        Some(curr_pressed_key)
+                    };
                 }
                 if delay_timer.wait().is_ok() {
                     computer.timer_tick();
